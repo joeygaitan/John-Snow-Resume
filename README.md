a8be56c6c93d8bb74cfde8a1834d09ec50488bff
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-04 11:38:20 -0700

    I added in a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+url: john-snow-resume.surge.sh
+
+John Snow Portfolio
--- test.py
+++
@@ -1,9 +0,0 @@
-def func(sam):
-    print(sam)
-    sam = ["sam", "smith", "hank"]
-    print(sam)
-
-x = ["sam", "smith"]
-print(x)
-func(x)
-print(x)
